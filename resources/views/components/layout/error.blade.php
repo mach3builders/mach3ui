e395ab3feffad64272b944d2a9e4779e2a24c04d
Paul Kruijt <pkruijt@mach3builders.nl>
2026-01-29 21:48:39 +0100

    refactor: Improve error layout structure and styling in error.blade.php

--- resources/views/components/layout/error.blade.php
+++ resources/views/components/layout/error.blade.php
@@ -10,23 +10,25 @@
 
 <div
     {{ $attributes->class([
-        'flex flex-col items-center text-center',
+        'flex flex-col items-center justify-center h-full text-center',
     ]) }}
     data-layout-error
 >
-    <h1 @class([
-        'text-[6rem] leading-none font-semibold tracking-tight',
-        'text-gray-800',
-        'dark:text-gray-100',
-    ])>{{ $title }}</h1>
+    <div class="w-full max-w-lg flex flex-col items-center">
+        <h1 @class([
+            'text-[6rem] leading-none font-semibold tracking-tight',
+            'text-gray-800',
+            'dark:text-gray-100',
+        ])>{{ $title }}</h1>
 
-    <p @class([
-        'mt-1 max-w-sm text-sm',
-        'text-gray-500',
-        'dark:text-gray-400',
-    ])>{{ $description }}</p>
+        <p @class([
+            'mt-1 max-w-sm text-sm',
+            'text-gray-500',
+            'dark:text-gray-400',
+        ])>{{ $description }}</p>
 
-    <div class="mt-8">
-        <ui:button href="/" icon="arrow-left">Go back</ui:button>
+        <div class="mt-8">
+            <ui:button href="/" icon="arrow-left">{{ __('common.go_back') }}</ui:button>
+        </div>
     </div>
 </div>
