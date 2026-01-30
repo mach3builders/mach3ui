1af0574bb5cea9dbfff4f0214d822441efa48ad5
Paul Kruijt <pkruijt@mach3builders.nl>
2026-01-30 11:48:52 +0100

    fix: Update danger button styles for improved hover effects

--- resources/views/components/nav/item.blade.php
+++ resources/views/components/nav/item.blade.php
@@ -18,7 +18,7 @@
     $base = 'flex items-center gap-2 rounded-md px-3 py-2 text-[13px] leading-5 [&>svg]:size-4 [&>svg]:shrink-0';
     $inactive = 'text-gray-600 hover:bg-black/5 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-white/5 dark:hover:text-gray-100 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-500 [&:hover>svg]:text-gray-500 dark:[&:hover>svg]:text-gray-400';
     $active_classes = 'bg-black/5 text-gray-900 dark:bg-white/5 dark:text-gray-100 [&>svg]:text-gray-500 dark:[&>svg]:text-gray-400';
-    $danger_classes = 'text-red-600 hover:text-red-700 dark:text-red-500 dark:hover:text-red-400 [&>svg]:text-red-500 [&:hover>svg]:text-red-600 dark:[&>svg]:text-red-600 dark:[&:hover>svg]:text-red-500';
+    $danger_classes = 'text-red-600 hover:bg-red-50 hover:text-red-700 dark:text-red-500 dark:hover:bg-red-950/50 dark:hover:text-red-400 [&>svg]:text-red-500 [&:hover>svg]:text-red-600 dark:[&>svg]:text-red-600 dark:[&:hover>svg]:text-red-500';
 
     if ($danger) {
         $classes = $base . ' ' . $danger_classes;
