8ed6837fb8001f6299be2504da243fc9e8b16340
Paul Kruijt <pkruijt@mach3builders.nl>
2026-02-01 19:12:33 +0100

    wip

--- resources/views/components/checkbox/card.blade.php
+++ resources/views/components/checkbox/card.blade.php
@@ -11,17 +11,14 @@
     $wireModel = $attributes->wire('model');
     $hasWireModel = $wireModel && method_exists($wireModel, 'value');
     $wireModelValue = $hasWireModel ? $wireModel->value() : null;
-
-    // Extract x-model attribute
-    $xModel = null;
-    foreach ($attributes as $key => $val) {
-        if (str_starts_with($key, 'x-model')) {
-            $xModel = $val;
-            break;
-        }
-    }
-
+    $xModel = collect($attributes->getAttributes())->first(fn($val, $key) => str_starts_with($key, 'x-model'));
     $name = $attributes->get('name') ?? ($wireModelValue ?? $xModel);
+
+    // SVG icons (fully URL encoded for Tailwind arbitrary values)
+    $checkmarkSvg =
+        "url('data:image/svg+xml,%3csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22white%22%20stroke-width%3D%223%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cpath%20d%3D%22M20%206%209%2017l-5-5%22%2F%3E%3C%2Fsvg%3E')";
+    $indeterminateSvg =
+        "url('data:image/svg+xml,%3csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22white%22%20stroke-width%3D%223%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cpath%20d%3D%22M5%2012h14%22%2F%3E%3C%2Fsvg%3E')";
 
     $cardClasses = Ui::classes()
         ->add('group relative flex cursor-pointer items-start gap-3 rounded-lg border p-4 transition-colors')
@@ -33,38 +30,31 @@
         ->add('dark:has-[:checked]:border-blue-500 dark:has-[:checked]:bg-blue-950/20')
         ->add('has-[:focus]:ring-2 has-[:focus]:ring-blue-600/20')
         ->add('dark:has-[:focus]:ring-blue-500/20')
-        ->add('has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50');
+        ->add('has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-50')
+        ->merge($attributes->only('class'));
 
     $checkboxClasses = Ui::classes()
         ->add('size-5 shrink-0 cursor-pointer appearance-none rounded-[5px] border bg-center bg-no-repeat')
         ->add('border-gray-300 bg-white')
         ->add('dark:border-gray-600 dark:bg-gray-800')
-        ->add('checked:border-blue-600 checked:bg-blue-600 checked:bg-[length:16px]')
+        ->add("checked:border-blue-600 checked:bg-blue-600 checked:bg-[length:16px] checked:bg-[{$checkmarkSvg}]")
         ->add('dark:checked:border-blue-500 dark:checked:bg-blue-500')
         ->add(
-            "checked:bg-[url('data:image/svg+xml,%3csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22white%22%20stroke-width%3D%223%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cpath%20d%3D%22M20%206%209%2017l-5-5%22%2F%3E%3C%2Fsvg%3E')]",
+            "indeterminate:border-blue-600 indeterminate:bg-blue-600 indeterminate:bg-[length:16px] indeterminate:bg-[{$indeterminateSvg}]",
         )
-        ->add('indeterminate:border-blue-600 indeterminate:bg-blue-600 indeterminate:bg-[length:16px]')
         ->add('dark:indeterminate:border-blue-500 dark:indeterminate:bg-blue-500')
-        ->add(
-            "indeterminate:bg-[url('data:image/svg+xml,%3csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22white%22%20stroke-width%3D%223%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3Cpath%20d%3D%22M5%2012h14%22%2F%3E%3C%2Fsvg%3E')]",
-        )
         ->add('focus:outline-none')
-        ->add('disabled:cursor-not-allowed disabled:opacity-50');
+        ->add('disabled:cursor-not-allowed disabled:opacity-50')
+        ->when($reversed, 'order-first');
 @endphp
 
-<label {{ $attributes->only('class')->class($cardClasses) }} data-checkbox-card data-control>
-    @if ($reversed)
-        <input type="checkbox" @if ($name) name="{{ $name }}" @endif
-            {{ $attributes->except(['class', 'name'])->class($checkboxClasses) }} @checked($checked)
-            @if ($indeterminate) x-init="$el.indeterminate = true" @endif data-checkbox />
-    @endif
-
+<label class="{{ $cardClasses }}" {{ $attributes->except('class') }} data-checkbox-card data-control>
     @if ($icon)
-        <ui:icon :name="$icon" class="size-6 shrink-0 text-gray-500 dark:text-gray-400" />
+        <ui:icon :name="$icon" class="size-6 shrink-0 text-gray-500 dark:text-gray-400"
+            data-checkbox-card-icon />
     @endif
 
-    <div class="flex-1">
+    <div class="flex-1" data-checkbox-card-content>
         @if ($title)
             <div class="text-sm font-medium text-gray-900 dark:text-gray-100">{{ $title }}</div>
         @endif
@@ -73,14 +63,10 @@
             <div class="mt-0.5 text-sm text-gray-500 dark:text-gray-400">{{ $description }}</div>
         @endif
 
-        @if ($slot->isNotEmpty())
-            {{ $slot }}
-        @endif
+        {{ $slot }}
     </div>
 
-    @if (!$reversed)
-        <input type="checkbox" @if ($name) name="{{ $name }}" @endif
-            {{ $attributes->except(['class', 'name'])->class($checkboxClasses) }} @checked($checked)
-            @if ($indeterminate) x-init="$el.indeterminate = true" @endif data-checkbox />
-    @endif
+    <input type="checkbox" @if ($name) name="{{ $name }}" @endif
+        @checked($checked) @if ($indeterminate) x-init="$el.indeterminate = true" @endif
+        class="{{ $checkboxClasses }}" data-checkbox />
 </label>
