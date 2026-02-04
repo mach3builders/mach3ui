c5cfe8fcccbe5eee86707fde14b628fe5be575de
Paul Kruijt <pkruijt@mach3builders.nl>
2026-02-04 12:53:47 +0100

    wip

--- resources/views/components/nav/item.blade.php
+++ resources/views/components/nav/item.blade.php
@@ -17,13 +17,13 @@
     $tag = $attributes->has('href') ? 'a' : 'button';
 
     $classes = Ui::classes()
-        ->add('flex cursor-pointer items-center gap-2 rounded-md px-3 py-2 text-[13px] leading-5')
+        ->add('flex cursor-pointer items-center text-left gap-2 rounded-md px-3 py-2 text-[13px] leading-5')
         ->add('[&>svg]:size-4 [&>svg]:shrink-0')
         ->add($variant, [
             'default' => $active
                 ? 'bg-black/5 text-gray-900 dark:bg-white/5 dark:text-gray-100 [&>svg]:text-gray-500 dark:[&>svg]:text-gray-400'
                 : 'text-gray-600 hover:bg-black/5 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-white/5 dark:hover:text-gray-100 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-500 [&:hover>svg]:text-gray-500 dark:[&:hover>svg]:text-gray-400',
-            'danger' => 'text-danger hover:bg-danger/10 dark:text-danger dark:hover:bg-danger/10',
+            'danger' => 'text-red-600 hover:bg-red-50 hover:text-red-700 dark:text-red-500 dark:hover:bg-red-950/50 dark:hover:text-red-400 [&>svg]:text-red-500 [&:hover>svg]:text-red-600 dark:[&>svg]:text-red-600 dark:[&:hover>svg]:text-red-500',
         ])
         ->when($disabled, 'pointer-events-none opacity-50')
         ->merge($attributes);
