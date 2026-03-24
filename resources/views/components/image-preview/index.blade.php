2cfc0e36a044ba96f2629bf913110f12e779f2bd
Paul Kruijt <pkruijt@mach3builders.nl>
2026-03-24 11:38:52 +0100

    New Image Preview component

---
+++ resources/views/components/image-preview/index.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'src' => null,
+    'alt' => '',
+    'delete' => null,
+])
+
+@php
+    $container_classes = Flux::classes()
+        ->add('flex h-20 w-32 items-center justify-center overflow-hidden rounded-lg border p-2')
+        ->add('border-zinc-200 bg-white dark:border-white/10 dark:bg-white/5');
+
+    $clickable_classes = Flux::classes($container_classes)
+        ->add('cursor-pointer hover:border-zinc-400 dark:hover:border-white/25');
+@endphp
+
+<div {{ $attributes->class('flex items-center gap-4') }} data-flux-image-preview>
+    @if ($src)
+        <button type="button" x-on:click="previewSrc = '{{ $src }}'; $flux.modal('image-preview').show()" class="{{ $clickable_classes }}">
+            <img src="{{ $src }}" alt="{{ $alt }}" class="max-h-full max-w-full rounded object-contain" />
+        </button>
+    @else
+        <div class="{{ $container_classes }}">
+            <flux:icon name="photo" class="size-5 text-zinc-300 dark:text-zinc-600" />
+        </div>
+    @endif
+
+    @if ($src && $delete)
+        <flux:button variant="subtle" size="sm" icon="trash-2" wire:click="{{ $delete }}">
+            {{ __('common.destroy') }}
+        </flux:button>
+    @endif
+</div>
---
+++ resources/views/components/image-preview/modal.blade.php
@@ -0,0 +1,3 @@
+<flux:modal name="image-preview" variant="bare" class="flex items-center justify-center p-8">
+    <img :src="previewSrc" class="max-h-[80vh] max-w-[80vw] rounded-lg object-contain" />
+</flux:modal>
